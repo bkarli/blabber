0ac0e22e3bde17eccc80879077048843ff66ab71
Benedikt Karli <benedikt.karli@gmail.com>
2026-07-10 14:39:23 +0200

    add blabber-core library

---
+++ blabber-core/src/identity.rs
@@ -0,0 +1,98 @@
+// Create store and manage Identities
+use rand::prelude::*;
+use anyhow::{anyhow, Context, Result};
+use chacha20poly1305::{
+    aead::{Aead, KeyInit},
+    ChaCha20Poly1305, Nonce,
+};
+use argon2::Argon2;
+
+const SALT_LEN: usize = 16;
+const NONCE_LEN: usize = 12;
+const KEY_LEN: usize = 32;
+
+
+/// Hold the displayName as well as the
+/// secret for generating the same node
+/// every time. 
+///
+/// TODO: We should mlock this one and use Zeroing
+pub struct Identity {
+    pub displayName: String,
+    pub secret: [u8; 32],
+}
+
+impl Identity {
+    
+    /// Create a new Identity from username and password
+    pub fn new(displayName: impl Into<String>) -> Self {
+        // create a random secret
+        let secret: [u8; 32] = rand::rng().random();
+        Self {
+            displayName: displayName.into(),
+            secret: secret,
+        }
+
+    }
+    
+    fn derive_key(password: &[u8], salt: &[u8]) -> Result<[u8; KEY_LEN]> {
+        let mut key = [0u8; KEY_LEN];
+        Argon2::default()
+            .hash_password_into(password, salt, &mut key)
+            .map_err(|e| anyhow!("Key derivation failed: {e}"))?;
+        Ok(key)
+    }
+
+    /// store the identity and encrypt with password
+    fn store(&self, password: impl Into<String>) -> Result<()> {
+        let salt: [u8; SALT_LEN] = rand::rng().random();
+        let nonce_bytes: [u8; NONCE_LEN] = rand::rng().random();
+
+        let password_bytes = password.into().as_bytes();
+        let mut key = derive_key(password_bytes, &salt)?;
+        let cypher = ChaCha20Poly1305::new((&key).into());
+        let nonce = Nonce::from_slice(&nonce_bytes);
+        
+        let plaintext = format!("{self.displayName}\n{self.secret}");
+
+        let ciphertext = cipher
+            .encrypt(nonce, plaintext)
+            .map_err(|e| anyhow!("encryption failed: {e}"))?;
+
+        let mut file = fs::File::create(path)
+        
+        Ok(())
+    }
+    //
+    // /// load the identity 
+    // pub fn load_from_disk(path: PathBuf) {
+    //
+    // }
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_identity_test() {
+        let id = Identity::new("Alice");
+        
+        // check if the display Name matches
+        assert_eq!(id.displayName, "Alice");
+
+        // check if the secret generated is actually random byte array
+        assert_ne!(id.secret, [0u8; 32]);
+
+    }
+
+    #[test]
+    fn store_identity_test() {
+        let id = Identity::new("Alice").store("SecurePassword");
+    }
+
+    #[test]
+    fn load_identity_test() {}
+}
