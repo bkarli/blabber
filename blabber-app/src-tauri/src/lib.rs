78188acc5b6001db1d79c4301e34eef2ebd40228
Benedikt Karli <benedikt.karli@gmail.com>
2026-07-28 11:58:36 +0200

    add plugin

--- blabber-app/src-tauri/src/lib.rs
+++ blabber-app/src-tauri/src/lib.rs
@@ -30,6 +30,7 @@ pub struct AppState {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        // .plugin(tauri_plugin_dialog::init())
         .manage(AppState::default())
         .invoke_handler(tauri::generate_handler![
             create_identity,
